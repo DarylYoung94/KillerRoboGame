ae698ed34b9ae74d631f5f6d696f3a7ca45fb7a8
GAllen425 <gallen.coding@gmail.com>
2020-05-30 08:52:13 +0100

    Conveyor Belt

---
+++ TestJungle/Assets/ConveyorBelt.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyorBelt : MonoBehaviour
+{
+    public float speed;
+
+    private void OnCollisionStay(Collision other)
+    {
+        other.transform.position += Vector3.left * speed * Time.deltaTime;
+    }
+}
