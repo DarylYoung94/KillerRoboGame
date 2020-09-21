42d3c2c9c30335bafc098f743471eaae5ce7f709
GAllen425 <gallen.coding@gmail.com>
2020-09-21 22:02:32 +0100

    Fix helicopter bugs
    - No longer applies end force to the last helicopter spawned
    - Set an angle clamp on the x rotation forward of the helicopter to prevent it diving and glitching out
    - A minimum height can now be specified so it doesnt hit the floor. Instead glides along it.

--- TestJungle/Assets/Scripts/HeliAbilityTriggerable.cs
+++ TestJungle/Assets/Scripts/HeliAbilityTriggerable.cs
@@ -9,14 +9,17 @@ public class HeliAbilityTriggerable : MonoBehaviour
     public float dropTimer = 4f;
    
     public GameObject dropPoint;
-    public bool Fired = false;
     public Rigidbody RB;
     [HideInInspector] public GameObject heliPrefab;
     public GameObject projectorPrefab;
     private GameObject projectorTarget;
     Vector3 heliAim = -Vector3.one;
     public float heliSpeed = 5f;
+    public float heightStart = 5.0f;
     public float despawnTimer = 7f;
+
+    List<Rigidbody> rigidbodies = new List<Rigidbody>();
+
     public void Hold()
     {
         UpdateProjector();
@@ -27,7 +30,7 @@ public class HeliAbilityTriggerable : MonoBehaviour
         Transform firepoint = this.gameObject.transform.Find("firePoint");
         if (projectorTarget == null)
         {
-            projectorTarget = Instantiate(projectorPrefab, firepoint.position , Quaternion.identity);
+            projectorTarget = Instantiate(projectorPrefab, firepoint.position + new Vector3(0,heightStart,0), Quaternion.identity);
         }
         else
         {
@@ -36,41 +39,37 @@ public class HeliAbilityTriggerable : MonoBehaviour
                 projectorTarget.gameObject.SetActive(true);
             }
             
-            projectorTarget.transform.position = firepoint.position;
+            projectorTarget.transform.position = firepoint.position + new Vector3(0,heightStart,0);
         }
     }
     
-        
-    
-     public void Release()
+    public void Release()
     {
-
-
         projectorTarget.SetActive(false);
 
         Transform firepoint = this.gameObject.transform.Find("firePoint");
-        GameObject HeliInstance = Instantiate(heliPrefab, firepoint.position, Quaternion.identity) as GameObject;
-        Rigidbody heliRB = HeliInstance.GetComponent<Rigidbody>();
-        RB = heliRB;
+        GameObject HeliInstance = Instantiate(heliPrefab, firepoint.position + new Vector3(0,heightStart,0), Quaternion.identity) as GameObject;
+        RB = HeliInstance.GetComponent<Rigidbody>();
+        rigidbodies.Add(RB);
+
         RaycastHit hit;
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
         if (Physics.Raycast(ray, out hit))
         {
             heliAim = hit.point;
-            
+
         }
 
         HeliInstance.transform.LookAt(heliAim);
 
-        heliRB.AddForce(heliRB.transform.forward * heliSpeed, ForceMode.Force);
-        StartCoroutine(ExecuteAfterTime(despawnTimer,HeliInstance,Fired));
-        
-         
+        RB.AddForce(RB.transform.forward * heliSpeed, ForceMode.Force);
+        StartCoroutine(ExecuteAfterTime(despawnTimer,HeliInstance));
     }
-    IEnumerator ExecuteAfterTime(float heliTime, GameObject heliInstance, bool Fired)
+
+    IEnumerator ExecuteAfterTime(float heliTime, GameObject heliInstance)
     {   
-        yield return new WaitForSeconds( heliTime -2);
+        yield return new WaitForSeconds(heliTime -2);
         EndHeli();
         yield return new WaitForSeconds(heliTime -3);
         Destroy(heliInstance);
@@ -78,8 +77,9 @@ public class HeliAbilityTriggerable : MonoBehaviour
     }
     
     void EndHeli()
-     {
-         RB.AddForce(RB.transform.up * heliSpeed*5, ForceMode.Force);
-     }
+    {
+        rigidbodies[0].AddForce(RB.transform.up * heliSpeed*5, ForceMode.Force);
+        rigidbodies.RemoveAt(0);
+    }
  
 }
--- TestJungle/Assets/Scripts/heliDrop.cs
+++ TestJungle/Assets/Scripts/heliDrop.cs
@@ -4,18 +4,60 @@ using UnityEngine;
 
 public class heliDrop : MonoBehaviour
 {
-   public GameObject droppedItem;
-   public float dropTimer = 4f;
-   
+    [Header ("Drop")]
+    public GameObject droppedItem;
+    public float dropTimer = 4f;
     public GameObject dropPoint;
 
-     void Start() 
+    [Header ("Helicopter")]
+    public float minHeight = 1.0f;
+    public float minRotationX = -45.0f, maxRotationX = 45.0f;
+    public bool clampRotationX = true;
+    public LayerMask layerMask;
+    public float heightLerpSpeed = 2.0f;
+    private float lerpTime = 0.0f;
+    private bool lerpingHeight = false;
+    private float desiredHeight;
+    private Transform heightChecker;
+    
+
+    void Start() 
     {
-       
         StartCoroutine(ExecuteAfterTime(dropTimer,droppedItem));
+        heightChecker = this.transform.Find("HeightChecker");
+    }
+
+    void FixedUpdate()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(heightChecker.position, Vector3.down, out hit, minHeight, layerMask))
+        {
+            float yMagnitude = Mathf.Clamp(heightChecker.position.y - hit.point.y, minHeight, Mathf.Infinity);
+            desiredHeight = this.transform.position.y + yMagnitude;
+            lerpingHeight = true;
+        }
+
+        if (lerpingHeight)
+        {
+            if(this.transform.position.y < desiredHeight)
+            {
+                float currentHeight =  Mathf.Lerp(this.transform.position.y, desiredHeight, Time.deltaTime * heightLerpSpeed);
+                this.transform.position = new Vector3(this.transform.position.x, currentHeight, this.transform.position.z);
+            }
+            else
+            {
+                lerpingHeight = false;
+            }
+        }
+
+        if (clampRotationX)
+        {
+            float xRotation = Mathf.Clamp(this.transform.localEulerAngles.x, minRotationX, maxRotationX);
+            this.transform.localEulerAngles = new Vector3 (xRotation, this.transform.localEulerAngles.y, this.transform.localEulerAngles.z);
+        }
     }
    
-   IEnumerator ExecuteAfterTime(float dropTimer, GameObject droppedItem) 
+    IEnumerator ExecuteAfterTime(float dropTimer, GameObject droppedItem) 
     {
         yield return new WaitForSeconds(dropTimer) ;
         SpawnDrop();
@@ -23,6 +65,6 @@ public class heliDrop : MonoBehaviour
 
     void SpawnDrop()
     {
-        GameObject droppedItemInstance = Instantiate(droppedItem, dropPoint.transform)as GameObject;
+        GameObject droppedItemInstance = Instantiate(droppedItem, dropPoint.transform) as GameObject;
     }
 }
