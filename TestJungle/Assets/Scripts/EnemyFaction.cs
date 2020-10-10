906e2ac1d7d7f4a7f4d2998e6b1b8aa41ec08690
DarylYoung94 <58364752+DarylYoung94@users.noreply.github.com>
2020-10-11 00:10:20 +0100

    Faction Start

---
+++ TestJungle/Assets/Scripts/EnemyFaction.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyFaction : MonoBehaviour
+{
+    public Factions factions;
+
+    public int disposition;
+
+    public int disFaction1;
+    public int disFaction2;
+    public int disFaction3;
+    public int disFaction4;
+        void Start()
+    {
+        disposition = factions.disposition;
+    
+    }
+     void Update()
+    {
+       disFaction1 = disposition - factions.faction1.disposition; 
+       disFaction2 = disposition - factions.faction2.disposition; 
+       disFaction3 = disposition - factions.faction3.disposition; 
+       disFaction4 = disposition - factions.faction4.disposition; 
+
+
+    }
+}
