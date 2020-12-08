957cb6d30ecb4c0664829d56d03eefc5b579886b
DarylYoung94 <58364752+DarylYoung94@users.noreply.github.com>
2020-12-08 20:21:11 +0000

    inventory ui

---
+++ TestJungle/Assets/Scripts/Inventory.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public bool inventoryEnabled;
+    public GameObject inventory;
+    
+    void Start()
+    {
+        inventory.SetActive(false);
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            inventoryEnabled= !inventoryEnabled;
+
+            if(inventoryEnabled == true)
+            
+                {
+                inventory.SetActive(true);
+                }
+                else
+                {
+                    inventory.SetActive(false);
+                }
+            
+        }
+            
+    }
+}
