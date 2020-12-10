43311f584490e4cbf8720df2efa5d6b6839705f8
GAllen425 <gallen.coding@gmail.com>
2020-12-10 17:24:32 +0000

    Fix bug in InvUIButton

--- TestJungle/Assets/Scripts/InvUIButtons.cs
+++ TestJungle/Assets/Scripts/InvUIButtons.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class InvUIButtons : MonoBehaviour
 {
     int clickedCount = 0;
+    bool moving = false;
     private Button clicked1, clicked2;
     private Vector3 position1, position2;
 
@@ -16,37 +17,31 @@ public class InvUIButtons : MonoBehaviour
     {
         if(clickedCount == 2)
         {
-            if(clicked1.tag == "AbilityItem"  && clicked2.tag == "InventoryItem")
-            {
-                Debug.Log("Tagged");
-                clicked2.tag = "AbilityItem";
-                clicked2.transform.parent = abilityParent;
-               
-                clicked1.tag = "InventoryItem";
-                clicked1.transform.parent = invParent;
-                
-               
-            } 
-
-            if(clicked1.tag == "InventoryItem" && clicked2.tag == "AbilityItem"  )
-            {
-                Debug.Log("Tagged");
-                clicked2.tag = "InventoryItem";
-                clicked2.transform.parent = invParent;
-
-                clicked1.tag = "AbilityItem";
-                clicked1.transform.parent = abilityParent;
-            }
-
-
             clicked1.transform.position = Vector3.Lerp(clicked1.transform.position, 
-            position2, 10f*Time.deltaTime); 
+                                                       position2,
+                                                       10f*Time.deltaTime); 
 
             clicked2.transform.position = Vector3.Lerp(clicked2.transform.position, 
-            position1, 10f*Time.deltaTime); 
-           
+                                                       position1,
+                                                       10f*Time.deltaTime); 
             
-            Invoke("ResetName", 1f);
+            if (!moving)
+            {
+                if (clicked1.tag != clicked2.tag)
+                {
+                    string tempTag = clicked1.tag;
+                    Transform tempTransform = clicked1.transform.parent;
+
+                    clicked1.tag = clicked2.tag;
+                    clicked1.transform.parent = clicked2.transform.parent;
+
+                    clicked2.tag = tempTag;
+                    clicked2.transform.parent = tempTransform;
+                }
+
+                moving = true;
+                Invoke("ResetName", 1f);
+            }
         }
     }
 
@@ -54,29 +49,28 @@ public class InvUIButtons : MonoBehaviour
     {
         clicked1.transform.localScale = new Vector2(1f,1f);
         clicked2.transform.localScale = new Vector2(1f,1f);
-        clickedCount =0; 
+
+        clickedCount = 0;
+        moving = false;
+
+
     }
-
-
 
    public void forBtn(Button btn)
    {
-       if(clickedCount ==0)
+       if(clickedCount == 0)
        {
            clicked1 = btn;
            position1 = btn.transform.position;
-           //btn.transform.name = "clicked";
            btn.transform.localScale = new Vector2(1.2f, 1.2f);
-            clickedCount++;
+           clickedCount++;
        }
-       
-       else if( clickedCount ==1)
+       else if( clickedCount == 1)
        {
            clicked2 = btn;
            position2 = btn.transform.position;
-           //btn.transform.name = "clicked";
            btn.transform.localScale = new Vector2(1.2f, 1.2f);
-            clickedCount++;
+           clickedCount++;
        }
    }
 }
